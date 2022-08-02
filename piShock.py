dc80bba9401d73d5f5c0ae4de57002bfa7ed3f8f
Faust <Jimmy.Lee.Tebo@gmail.com>
2022-08-02 11:25:36 -0500

    piShock api quickly setup

--- piShock.py
+++ piShock.py
@@ -5,11 +5,27 @@ class piShock:
     apikey: str = None
     code: str = None
     name: str = None
-    def __init__(self, username, apikey, code, name):
-        if username == None or apikey == None or code == None or name == None:
+    operation: str = None
+    intensity: int = None
+    duration: int = None
+    def __init__(self, username, apikey, code, name, operation, intensity, duration):
+        if username == None or apikey == None or code == None or name == None or operation == None or intensity == None or duration == None:
             """Missing required parameters"""
         elif name == "PiShock":
             """'name' must be different from PiShock"""
+        elif duration > 10 or duration < 1:
+            """Duration must be between 1 and 10"""
+        elif intensity > 100 or intensity < 1:
+            """Intensity must be between 1 and 100"""
         else:
             session = requests.Session()
-
+            session.params = {}
+            session.params['Username'] = username
+            session.params['Name'] = name
+            session.params['Code'] = code
+            session.params['Intensity'] = intensity
+            session.params['Duration'] = duration
+            session.params['Apikey'] = apikey
+            session.params['Op'] = operation
+            response = session.post('https://do.pishock.com/api/apioperate', json = session.params)
+            response.json()
